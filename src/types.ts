e6b06d3794f33262c20905b45430d7b9b25d5ac9
glennmichael123 <gtorregosa@gmail.com>
2025-05-14 20:24:02 +0800

    feat: types

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,12 @@
 import type { alphanumeric } from './lib/isAlphanumeric'
+import type { ArrayValidator } from './validators/arrays'
+import type { BooleanValidator } from './validators/booleans'
+import type { CustomValidator } from './validators/custom'
+import type { DateValidator } from './validators/dates'
+import type { EnumValidator } from './validators/enums'
+import type { NumberValidator } from './validators/numbers'
+import type { ObjectValidator } from './validators/objects'
+import type { StringValidator } from './validators/strings'
 
 export interface ValidationError {
   message: string
@@ -234,4 +242,15 @@ export interface ValidationConfig {
   strictMode?: boolean
   cacheResults?: boolean
   errorMessages?: Record<string, string>
+}
+
+export interface ValidationInstance {
+  string: () => StringValidator
+  number: () => NumberValidator
+  array: <T>() => ArrayValidator<T>
+  boolean: () => BooleanValidator
+  enum: <T extends string | number>(values: readonly T[]) => EnumValidator<T>
+  date: () => DateValidator
+  object: <T extends Record<string, any>>() => ObjectValidator<T>
+  custom: <T>(validationFn: (value: T) => boolean, message: string) => CustomValidator<T>
 }
--- src/validation.ts
+++ src/validation.ts
@@ -1,12 +1,4 @@
-import type { ArrayValidator } from './validators/arrays'
-import type { BooleanValidator } from './validators/booleans'
-import type { CustomValidator } from './validators/custom'
-import type { DateValidator } from './validators/dates'
-import type { EnumValidator } from './validators/enums'
-import type { NumberValidator } from './validators/numbers'
-import type { ObjectValidator } from './validators/objects'
-import type { StringValidator } from './validators/strings'
-
+import type { ValidationInstance } from './types'
 import { array } from './validators/arrays'
 import { boolean } from './validators/booleans'
 import { custom } from './validators/custom'
@@ -16,18 +8,7 @@ import { number } from './validators/numbers'
 import { object } from './validators/objects'
 import { string } from './validators/strings'
 
-interface Validator {
-  string: () => StringValidator
-  number: () => NumberValidator
-  array: <T>() => ArrayValidator<T>
-  boolean: () => BooleanValidator
-  enum: <T extends string | number>(values: readonly T[]) => EnumValidator<T>
-  date: () => DateValidator
-  object: <T extends Record<string, any>>() => ObjectValidator<T>
-  custom: <T>(validationFn: (value: T) => boolean, message: string) => CustomValidator<T>
-}
-
-export const v: Validator = {
+export const v: ValidationInstance = {
   string,
   number,
   array,
